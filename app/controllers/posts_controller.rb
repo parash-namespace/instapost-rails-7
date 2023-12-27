f4ccf0feeacd0f611867a4dbfbda85b5234c8abc
parash-namespace <parash.a@namespace.jp>
2023-12-27 16:42:51 +0545

    Add navbar and posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
   before_action :authenticate_user, only: [:new, :create]
 
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
   def new
     @post = Post.new
   end
 
   def create
     @post = current_user.posts.create(post_params)
+
     if @post.valid?
       redirect_to root_path
     else
@@ -17,6 +22,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:user_id, :photo, :caption)
+    params.require(:post).permit(:user_id, :photo, :description)
   end
 end
