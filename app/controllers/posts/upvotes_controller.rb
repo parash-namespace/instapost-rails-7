84d600923bc7f308cd9488f41c56b640deb49c3e
parash-namespace <parash.a@namespace.jp>
2024-01-24 14:42:14 +0545

    Implement upvote and downvote create method

--- app/controllers/posts/upvotes_controller.rb
+++ app/controllers/posts/upvotes_controller.rb
@@ -1,5 +1,10 @@
 class Posts::UpvotesController < ApplicationController
   def create
-    debugger
+    @post = Post.find(params[:post_id])
+    @post.liked_by current_user
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("post_#{params[:post_id]}_container", partial: 'posts/likes', locals: { post: @post }) }
+    end
   end
 end
